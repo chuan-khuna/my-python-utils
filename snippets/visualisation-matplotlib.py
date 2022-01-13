52885e24243a8b6222f90b1acfa1c1593bc6e90c
ALTR <chuan.khuna@gmail.com>
2022-01-13 14:58:45 +0700

    Update Visualisation theme

--- snippets/visualisation-matplotlib.py
+++ snippets/visualisation-matplotlib.py
@@ -4,6 +4,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+_30k_main_palette_dict = {
+    "blue": "#202F66",
+    "orange": "#FF7048",
+    "purple": "#8B87EA",
+    "yellow": "#F3D36E",
+    "pink": "#D869AB",
+    "cyan": "#54C9B9",
+}
+
+_30k_danger_safe_palette_dict = {
+    "danger": "#FF4F72",
+    "safe": "#58EDB9",
+}
+
+_30k_text_palette_dict = {
+    "text_black": "#32363A",
+    "text_lighter1": "#6F7273",
+    "text_lighter2": "#8B8E8F",
+    "bg_cream": "#FAF7F4"
+}
+
 _font_mono = "Inconsolata"
 _font_serif = "Canela Desk"
 _font_sans = "Gill Sans Nova"
@@ -14,15 +35,21 @@ sns.set_theme(style="whitegrid",
               rc={
                   "figure.figsize": (10.5, 4.5),
                   "figure.dpi": 300,
+                  "savefig.dpi": 300,
                   "grid.alpha": 0.1,
                   "grid.color": "#1b262c",
                   "grid.linewidth": 0.5,
                   "font.family": _font_mono,
                   "lines.solid_capstyle": "projecting",
                   "lines.solid_joinstyle": "bevel",
+                  "axes.facecolor": "#FAF7F4",
+                  "figure.facecolor": "#FAF7F4",
+                  "savefig.facecolor": "#FAF7F4",
+                  "savefig.bbox": "tight",
+                  "savefig.pad_inches": 0.1,
               })
 
-_30k = ["#202f66", "#ff7048", "#7f68d0", "#f3d36e", "#d869ab", "#48ADA9", "#1b262c"]
+_30k = list(_30k_main_palette_dict.values())
 sns.set_palette(_30k)
 
 import warnings
